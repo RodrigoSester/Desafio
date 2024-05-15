d8f0cee74cff98494d7f775c59c62b38232056dc
Rodrigo <rsesterheim@maisaedu.com.br>
2024-05-15 12:55:25 -0300

    feat(candidates-info): implement logic to generate random candidates with informations

--- src/SecondStep/Classes/PrincipalCandidatos.java
+++ src/SecondStep/Classes/PrincipalCandidatos.java
@@ -4,14 +4,34 @@ import java.util.*;
 
 public class PrincipalCandidatos {
 	private static Random random = new Random();
-	private static int[] candidatos;
+	private static Candidato[] candidatos;
 
 	public static void main(String args[]) {
-		int randomNumber = random.nextInt(1, 100);
-		System.out.println(randomNumber);
+		int numCandidates = random.nextInt(1, 100);
+		System.out.println(numCandidates);
 		
-		candidatos = new int[randomNumber];
+		candidatos = new Candidato[numCandidates];
 	}
+	
+	private static void generateCandidates(int numCandidates) {
+        for (int i = 0; i < numCandidates; i++) {
+            String name = generateRandomName();
+            String party = generateRandomParty();
+            int votes = random.nextInt(100000);
+            candidatos[i] = new Candidato(name, party, votes);
+        }
+    }
+
+    private static String generateRandomName() {
+        String[] names = {"Alice", "Bob", "Charlie", "David", "Emily", "Hope", "Maggy", "Homer", "Lau"};
+        return names[random.nextInt(names.length)];
+    }
+
+    private static String generateRandomParty() {
+        String[] parties = {"PT", "PSDB", "DEM", "PP", "PSOL", "PM"};
+        return parties[random.nextInt(parties.length)];
+    }
+
 	
 	
 }
