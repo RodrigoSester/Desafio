f080c2dffc9c61e37fb28139f206d12df6c6ffde
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-06-16 17:36:56 -0300

    feat: handle return to log result

--- src/ThirdStep/Main.java
+++ src/ThirdStep/Main.java
@@ -6,17 +6,19 @@ public class Main {
 	private static Stack<Character> stack = new Stack<>();
 	private static Etapa3 thirdStep = new Etapa3();
 
-	private static String expression = ")A+B(-C";
+	private static String expression = "(A+B)-C";
 	
 	public static void main(String[] args) {
 		char[] characters = expression.toCharArray();
-		
+
 		for (int index = characters.length - 1; index >= 0; index--) {
 			stack.add(characters[index]);
 		}
 		
-		System.out.println(stack);
-		
-		System.out.println(thirdStep.checkBrackets(stack));
+		if (thirdStep.checkBrackets(stack)) {
+			System.out.println("Bom! Sua fórmula matemática está balanceada!");
+		} else {
+			System.out.println("Não está correto! Verifique a fórmula e tente novamente!");
+		}
 	}
 }
